08c1b4859e60d1334503e3803128a10d98c9221a
Rabi Róbert <ingenmaffen@gmail.com>
2019-09-16 22:37:52 +0200

    Adds favicon and gitignore

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 768,
+        icon: './favicon.png',
         webPreferences: {
             nodeIntegration: true
         }

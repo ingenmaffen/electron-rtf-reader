5db0fcf856dea24814434ff5ff647ae8b3d6b7c0
Rabi Róbert <ingenmaffen@gmail.com>
2020-01-30 22:08:16 +0100

    Add dark theme

--- main.js
+++ main.js
@@ -1,85 +1,115 @@
-const { app, BrowserWindow, Menu, dialog, ipcMain } = require('electron');
-const fs = require('fs');
+const { app, BrowserWindow, Menu, dialog, ipcMain } = require("electron");
+const fs = require("fs");
 
-const userPath = app.getPath('userData');
+const userPath = app.getPath("userData");
 
 const mainMenu = Menu.buildFromTemplate([
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Open',
-                click: () => {
-                    dialog.showOpenDialog(mainWindow, {
-                        filters: 
-                        {
-                            name: 'Rich Text File',
-                            extensions: ['rtf'] 
-                        }
-                        
-                    }, filepath => {
-                        if (filepath && filepath.length) { 
-                            mainWindow.webContents.send('path', filepath.toString());
-                            fs.writeFile(userPath + '/filepath', filepath.toString(), () => {});
-                        }
-                    });
-                }
-            },
-            {
-                role: 'quit'
-            }
-        ]
-    },
-    {
-        label: 'Find',
-        accelerator: 'CmdOrCtrl+F',
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Open",
         click: () => {
-            searchWindow.show();
-
+          dialog.showOpenDialog(
+            mainWindow,
+            {
+              filters: {
+                name: "Rich Text File",
+                extensions: ["rtf"]
+              }
+            },
+            filepath => {
+              if (filepath && filepath.length) {
+                mainWindow.webContents.send("path", filepath.toString());
+                fs.writeFile(
+                  userPath + "/filepath",
+                  filepath.toString(),
+                  () => {}
+                );
+              }
+            }
+          );
         }
-    }
+      },
+      {
+        role: "quit"
+      }
+    ]
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Find",
+        accelerator: "CmdOrCtrl+F",
+        click: () => {
+          searchWindow.show();
+        }
+      },
+      {
+        label: "Theme",
+        submenu: [
+          {
+            label: "Light",
+            click: () => {
+              mainWindow.webContents.send("theme", "light");
+            }
+          },
+          {
+            label: "Dark",
+            click: () => {
+              mainWindow.webContents.send("theme", "dark");
+            }
+          }
+        ]
+      }
+    ]
+  }
 ]);
 
-
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        minWidth: 800,
-        minHeight: 600,
-        icon: './favicon.png',
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    
-    mainWindow.loadFile('index.html');
-    mainWindow.webContents.on('did-finish-load', () => {
-        fs.readFile(userPath + '/filepath', (err, data) => {
-            if (data) mainWindow.webContents.send('path', data.toString());
-        });
-    }); 
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    minWidth: 800,
+    minHeight: 600,
+    icon: "./favicon.png",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    searchWindow = new BrowserWindow({
-        width: 300,
-        height: 100,
-        parent: mainWindow,
-        modal: true,
-        show: false,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
+  mainWindow.loadFile("index.html");
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("load-theme", null);
+  });
+  mainWindow.webContents.on("did-finish-load", () => {
+    fs.readFile(userPath + "/filepath", (err, data) => {
+      if (data) mainWindow.webContents.send("path", data.toString());
     });
-    
-    searchWindow.loadFile('search.html');
-    ipcMain.on('search', (_event, message) => {
-        searchWindow.hide();
-        mainWindow.webContents.send('search', message);
-    });
+  });
 
+  mainWindow.openDevTools();
 
-    Menu.setApplicationMenu(mainMenu);
+  searchWindow = new BrowserWindow({
+    width: 300,
+    height: 100,
+    parent: mainWindow,
+    modal: true,
+    show: false,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  searchWindow.loadFile("search.html");
+  ipcMain.on("search", (_event, message) => {
+    searchWindow.hide();
+    mainWindow.webContents.send("search", message);
+  });
+
+  Menu.setApplicationMenu(mainMenu);
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
--- renderer.js
+++ renderer.js
@@ -1,36 +1,61 @@
-const rtfReader = require('./rtf-import');
-const fs = require('fs');
-const {ipcRenderer} = require('electron');
+const rtfReader = require("./rtf-import");
+const fs = require("fs");
+const { ipcRenderer, session } = require("electron");
 
-ipcRenderer.on('path', function(_event, message) {
-    readFile(message)
+ipcRenderer.on("path", function(_event, message) {
+  readFile(message);
 });
 
-ipcRenderer.on('search', function(_event, message) {
-    findText(message)
+ipcRenderer.on("search", function(_event, message) {
+  findText(message);
 });
 
+ipcRenderer.on("load-theme", function(_event, _message) {
+  const theme = window.localStorage.getItem("theme");
+  setTheme(theme);
+});
+
+ipcRenderer.on("theme", function(_event, message) {
+  window.localStorage.setItem("theme", message);
+  setTheme(message);
+});
 
 function readFile(path) {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if(err){
-            alert("An error ocurred reading the file :" + err.message);
-            return;
-        }
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      alert("An error ocurred reading the file :" + err.message);
+      return;
+    }
 
-        rtfReader.runRtfjs(data, onFileParse, (error) => {
-        alert(error);
-        });
+    rtfReader.runRtfjs(data, onFileParse, error => {
+      alert(error);
     });
+  });
 }
 
-
 function onFileParse(_metaData, html) {
-    const contentDiv = document.getElementById('content');
-    contentDiv.innerHTML = "";
-    contentDiv.insertAdjacentHTML( 'beforeend', html );
+  const contentDiv = document.getElementById("content");
+  contentDiv.innerHTML = "";
+  contentDiv.insertAdjacentHTML("beforeend", html);
 }
 
 function findText(text) {
-    window.find(text);
+  window.find(text);
+}
+
+function setTheme(theme) {
+  switch (theme) {
+    case "light":
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      break;
+    case "dark":
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      break;
+    default:
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      break;
+  }
 }
